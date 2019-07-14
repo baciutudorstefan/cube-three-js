5eb3f38173728bc1b16810b36819c0d9ae74f9ac
Baciu Tudor Stefan <47207902+baciutudorstefan@users.noreply.github.com>
2019-07-14 17:25:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 cube
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 100, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
